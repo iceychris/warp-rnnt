d1f2235739af96972ddc9bbd14fe6dc643cea4af
Christian <12550267+iceychris@users.noreply.github.com>
2022-05-31 12:32:28 +0200

    skip check for gpu (useful in docker builds)

--- pytorch_binding/setup.py
+++ pytorch_binding/setup.py
@@ -18,8 +18,9 @@ def get_long_description():
         return f.read()
 
 
-if not torch.cuda.is_available():
-    raise Exception("CPU version is not implemented")
+# skip check for gpu (useful in docker builds)
+# if not torch.cuda.is_available():
+#     raise Exception("CPU version is not implemented")
 
 
 requirements = get_requirements()
